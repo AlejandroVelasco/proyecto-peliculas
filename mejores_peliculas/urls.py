4f8d0ff7e7b0ad1c41af3e72ff571521f64423c1
Alejandro Velasco <aevelasco@ibw.com>
2023-10-23 18:03:19 -0600

    ✨ Detalle peliculas

--- mejores_peliculas/urls.py
+++ mejores_peliculas/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="pagina-inicial"),
+    path("ranking", views.index, name="pagina-ranking"),
+    path("<int:id>", views.detalle, name="pagina-detalle"),
 ]
--- mejores_peliculas/views.py
+++ mejores_peliculas/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "mejores_peliculas/index.html")
+
+def detalle(request, id):    
+    return render(request, "mejores_peliculas/detalle.html")
